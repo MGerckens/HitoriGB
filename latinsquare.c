505247494656bc1a0e4c3328bb08bf33bdf27682
MGerckens <63205374+MGerckens@users.noreply.github.com>
2021-11-19 15:48:21 -0500

    Fixed same order issue
    Previously, every row had the same order with a random offset. This should fix that.

--- latinsquare.c
+++ latinsquare.c
@@ -10,19 +10,28 @@
 #define SWAPS 30
 
 
+static inline void swap_cols(uint8_t col1, uint8_t col2){ //swaps two cols on the board
+	uint8_t temp;
+    for(uint8_t i = 0; i < board_size; i++){
+        temp = board[col1*board_size + i];
+        board[col1*board_size + i] = board[col2*board_size + i];
+        board[col2*board_size + i] = temp;
+    }
+}
+
 static inline void swap_rows(uint8_t row1, uint8_t row2){ //swaps two rows on the board
 	uint8_t temp;
     for(uint8_t i = 0; i < board_size; i++){
-        temp = board[row1*board_size + i];
-        board[row1*board_size + i] = board[row2*board_size + i];
-        board[row2*board_size + i] = temp;
+        temp = board[i*board_size + row1];
+        board[i*board_size + row1] = board[i*board_size + row2];
+        board[i*board_size + row2] = temp;
     }
 }
 
 
 void latin_generate(){
 	initarand(clock());
-    uint8_t row1, row2, i, j, rand, temp;
+    uint8_t index1, index2, i, j, rand, temp;
 	uint8_t *randlist = (uint8_t *)malloc(board_size * sizeof(uint8_t));
 	
 	for(i = 0; i < board_size; i++){ //initialize list of numbers
@@ -43,8 +52,9 @@ void latin_generate(){
     }
 
     for(i = 0; i < SWAPS; i++){ //shuffle the rows, to be less predictable
-        row1 = (uint8_t)arand() % board_size;
-        do{row2 = (uint8_t)arand() % board_size;}while(row2 == row1);
-        swap_rows(row1, row2);
+        index1 = (uint8_t)arand() % board_size;
+        do{index2 = (uint8_t)arand() % board_size;}while(index1 == index2);
+        swap_rows(index1, index2);
+		swap_cols(index1, index2);
     }
 }
